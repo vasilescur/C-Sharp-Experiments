26d87653f12d36220d6e1840c8f88f63a7909d3d
Radu Vasilescu <vasilescur@gmail.com>
2017-08-12 08:46:51 -0400

    Add files via upload

---
+++ DoNothing/DoNothing/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DoNothing
+{
+    public partial class Form1 : Form
+    {
+        private Thread nullWorker = new Thread(() => { while (true) { } });
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!nullWorker.IsAlive) nullWorker.Start();
+        } 
+
+        private void button2_Click(object sender, EventArgs e) => nullWorker.Abort();
+    }
+}
